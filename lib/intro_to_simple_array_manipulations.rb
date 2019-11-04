b80fcd8ffcccd2b074a4d66c0e588263585aab70
Basri Dogan <basri.dogan.71@gmail.com>
2019-11-04 10:13:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,15 @@
-def using_push(Array, String)
-  Array.push(String)
+def using_push(my_Array, my_String)
+  my_Array.push(my_String)
 end 
+
+def using_unshift(m_Array,m_String)
+  m_Array.unshift(m_String)
+end
+
+def using_pop(m_Array)
+  m_Array.pop
+end
+
+def pop_with_args(m_Array,m_count)
+  m_Array
+end
